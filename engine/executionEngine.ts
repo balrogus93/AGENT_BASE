a8410a0f4d99175dabb9d96d44ad9e56819ddba7
Balrogus93 <119085332+balrogus93@users.noreply.github.com>
2026-02-21 22:35:29 +0100

    Create executionEngine.ts

---
+++ engine/executionEngine.ts
@@ -0,0 +1,15 @@
+import { sendTransaction } from "@/lib/wallet";
+
+export async function executeRebalance(from: any, to: any) {
+  console.log(`Rebalancing from ${from.name} to ${to.name}`);
+
+  await sendTransaction({
+    action: "withdraw",
+    protocol: from.name
+  });
+
+  await sendTransaction({
+    action: "deposit",
+    protocol: to.name
+  });
+}
